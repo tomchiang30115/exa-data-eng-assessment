ceafb2aae4d7ff333a7faf9be020e62a3c39af18
Thomas Chiang <tomchiang30115@gmail.com>
2023-07-24 11:01:06 +0100

    adding more comments

--- FHIR_data_loader.py
+++ FHIR_data_loader.py
@@ -1,3 +1,4 @@
+# Import necessary libraries
 import os
 import pandas as pd
 import json
@@ -7,45 +8,54 @@ import psycopg2.extras
 import numpy as np
 import argparse
 
-
+# Define a function to read nested JSON data from a file
 def read_nested_json(file_path):
-    # Extract json files from allocated file path and return json format data
+    # Open the file and load JSON data into a Python dictionary
     with open(file_path, "r", encoding="utf8") as file:
         data = json.load(file)
     return data
 
-
+# Define a function to normalize nested JSON data using pandas json_normalize()
 def normalize_nested_json(data):
     # Use pandas json_normalize() to flatten the nested JSON data
+    # The 'record_path' parameter specifies the path to the nested records in the JSON
+    # The 'max_level' parameter limits the depth of normalization to level 2
+    # The 'sep' parameter is used to separate the column names when flattening nested dictionaries
     df = pd.json_normalize(data, record_path=["entry"], max_level=2, sep="_")
     return df
 
-
+# Define a function to process all JSON files in a directory
 def process_json_files(json_directory):
 
+    # Create an empty list to store DataFrames from all JSON files
     all_dataframes = []
 
     # Iterate through all files in the directory
     for filename in os.listdir(json_directory):
+        # Check if the file has a '.json' extension
         if filename.endswith(".json"):
+            # Create the full file path
             file_path = os.path.join(json_directory, filename)
+            # Read the nested JSON data from the file
             nested_json_data = read_nested_json(file_path)
+            # Normalize the nested JSON data and get a DataFrame
             normalized_df = normalize_nested_json(nested_json_data)
+            # Append the DataFrame to the list
             all_dataframes.append(normalized_df)
 
-    # Concatenate all dataframes into a single dataframe
+    # Concatenate all DataFrames into a single DataFrame
     combined_df = pd.concat(all_dataframes, ignore_index=True)
     return combined_df
 
-
+# Define a function to create individual DataFrames for each resource type
 def create_resource_dataframes(combined_df):
-
+    # Get a list of unique resource types from the 'resource_resourceType' column
     resourceType_lst = combined_df["resource_resourceType"].unique().tolist()
 
-    # Create an empty dictionary to store the dataframes
+    # Create an empty dictionary to store the DataFrames
     dataframes_dict = {}
 
-    # Loop through each resource type and create the respective dataframe
+    # Loop through each resource type and create the respective DataFrame
     for rT in resourceType_lst:
         # Filter the rows for the current resource type and drop columns with NaN values
         df = (
@@ -54,12 +64,12 @@ def create_resource_dataframes(combined_df):
             .convert_dtypes()
         )
 
-        # Store the dataframe in the dictionary with the resource type as the key
+        # Store the DataFrame in the dictionary with the resource type as the key
         dataframes_dict[rT] = df
 
     return dataframes_dict
 
-
+# Define a function to create a new PostgreSQL database
 def create_database(db_name, db_user, db_password, db_host, db_port):
     try:
         # Connect to the default "postgres" database first to create a new database
@@ -95,12 +105,12 @@ def create_database(db_name, db_user, db_password, db_host, db_port):
     except psycopg2.Error as e:
         print("Error:", e)
 
-
+# Define a function to create a PostgreSQL table and insert data from a DataFrame in bulk
 def create_table_from_dataframe_bulk(
     df, table_name, db_name, db_user, db_password, db_host, db_port
 ):
     try:
-        # Connect to the 'fhir_db' database
+        # Connect to the specified database
         conn = psycopg2.connect(
             dbname=db_name,
             user=db_user,
@@ -153,8 +163,7 @@ def create_table_from_dataframe_bulk(
     except psycopg2.Error as e:
         print("Error:", e)
 
-
-# Replace the data_type_mapping function with a dictionary mapping of data types
+# Dictionary mapping of data types used for creating PostgreSQL table columns
 data_type_mapping = {
     "object": "TEXT",
     "int64": "BIGINT",
@@ -170,15 +179,18 @@ data_type_mapping = {
     # Add other mappings as needed
 }
 
-
+# Define a function to parse command-line arguments
 def parse_arguments():
     parser = argparse.ArgumentParser(description="FHIR Data Loader Script")
 
+    # Add required arguments for database credentials
     parser.add_argument(
         "--database_name", required=True, help="Name of the new database"
     )
     parser.add_argument("--database_user", required=True, help="Database user")
     parser.add_argument("--database_password", required=True, help="Database password")
+
+    # Add optional arguments for database host and port
     parser.add_argument(
         "--database_host",
         default="localhost",
@@ -190,36 +202,33 @@ def parse_arguments():
 
     return parser.parse_args()
 
+# Main code starts here
 
 if __name__ == "__main__":
 
     # Add a timer to measure the script's execution time
     start_time = time.time()
 
+    # Set the path to the directory containing JSON files
     json_directory = (
         "data"  # Replace this with the path to your directory containing JSON files
     )
 
+    # Process all JSON files and combine them into a single DataFrame
     combined_df = process_json_files(json_directory)
+
+    # Create individual DataFrames for each resource type and store them in a dictionary
     dataframes_dict = create_resource_dataframes(combined_df)
 
-    # Replace these variables with your actual database credentials
-    # new_database_name = "FHIR_DB"
-    # database_user = "postgres"
-    # database_password = "password0123"
-    # database_host = "localhost"  # Typically "localhost" if running locally, change it to host.docker.internal if building docker image
-    # database_port = 5432  # Replace this with the correct integer port number
-
+    # Parse command-line arguments to get database credentials
     args = parse_arguments()
-
-    # Access the credentials from the command-line arguments
     new_database_name = args.database_name
     database_user = args.database_user
     database_password = args.database_password
     database_host = args.database_host
     database_port = args.database_port
 
-    # Check if the database already exists
+    # Check if the database already exists or create a new one
     create_database(
         new_database_name,
         database_user,
@@ -228,7 +237,7 @@ if __name__ == "__main__":
         database_port,
     )
 
-    # Assuming dataframes_dict contains the DataFrames
+    # List of resource types to process
     resourceType = [
         "Encounter",
         "Patient",
@@ -244,8 +253,11 @@ if __name__ == "__main__":
         "Provenance",
         "Device",
     ]
+
+    # Filter the dictionary to include only the desired resource types
     dataframes_dict = {restype: dataframes_dict[restype] for restype in resourceType}
 
+    # Database name where tables will be created
     database_name = "fhir_db"
 
     # Iterate through the DataFrames and insert them into corresponding tables
@@ -261,7 +273,6 @@ if __name__ == "__main__":
         )
 
     # Calculate and print the execution time
-
     end_time = time.time()
     execution_time = end_time - start_time
     print(f"Script executed in {execution_time:.2f} seconds.")
